e5ae6730c0f5073968f5588a0fdb0c6f90407faf
rbdeveloppement <rbdeveloppement@gmail.com>
2022-10-13 16:08:21 +0200

    TP5

--- controllers/auth.controller.php
+++ controllers/auth.controller.php
@@ -1,12 +1,17 @@
-l<?php
+<?php
+
+use Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
+
+
 class AuthController
 {
     public function __construct($params)
     {
-        $methode = array_shift($params);
+        $this->methode = array_shift($params);
         $this->action = null;
 
-        if(!isset($methode) ){
+        if(!isset($this->methode) ){
             return $this;
         }
         
@@ -17,6 +22,10 @@ class AuthController
         
         if($_SERVER['REQUEST_METHOD'] == "POST"){
             $this->action = $this->login();
+        }
+
+        if($_SERVER['REQUEST_METHOD'] == "GET"){
+            $this->action = $this->check();
         }
 }
 public function login()
@@ -31,11 +40,52 @@ public function login()
     $accounts = $dbs->selectWhere($where, [$email, 0]);
     $account = $accounts[0];
     $prefix = $_ENV['config']->hash->prefix;
-    if(isset($account) && password_verify($account->password == $password, $prefix)){
+    if(count($accounts) == 1
+       && password_verify($this->body['password'], $prefix . $accounts[0]->password)){
        $dbs = new DatabaseService("appUser");
        $appUser = $dbs->selectOne($account->Id_appUser);
-       return ["result" => true, "role" => $appUser->Id_role];
+        $secretKey = $_ENV['config']->jwt->secret;  //définie la clé
+        $issuedAt = time();                        // durée de conservation du token
+        $expireAt = $issuedAt + 60 * 60 *24;        // seconde * minutes * heures
+        $serverName = "blog.api";                   // nom du serveur
+        $userRole = $appUser->Id_role;
+        $userId = $appUser->Id_appUser;
+        $requestData = [
+            'iat' => $issuedAt,
+            'iss' => $serverName,
+            'nbf' => $issuedAt,
+            'exp' => $expireAt,
+            'userRole' => $userRole,
+            'userid' => $userId
+        ];
+        $token = JWT::encode($requestData, $secretKey, 'HS512');
+
+       return ["result" => true, "role" => $appUser->Id_role, "id" => $appUser->Id_appUser, "token" => $token];
     }
+    return ["result" => false];
+}
+
+public function check(){
+    $headers = apache_request_headers();
+    if (isset($headers["Authorizations"])) {
+        $token = $headers["Authorization"];
+    }
+    $secretKey = $_ENV['config']->jwt->secret;
+    if (isset($token) && !empty($token)) {
+        try{
+            $payload = JWT::decode($token, new Key($secretKey, 'HS512'));     //HS512 correspond à un algo de hachage plus de plus sécurisé
+        }catch(Exception $e){
+            $payload = null;
+        }
+        if (isset($payload) &&
+        $payload->iss === "blog.api" &&    //iss doit avoir le meme nom que $serverName
+        $payload->nbf < time() &&
+        $payload->exp > time())
+        {
+            return ["result" => true, "role" => $payload->userRole, "id" => $payload->userId];
+        }
+    }
+
     return ["result" => false];
 }
 }
--- index.php
+++ index.php
@@ -1,68 +1,72 @@
-<?php 
+<?php
 
-    $_ENV["current"] = "dev"; 
-    $config = file_get_contents("configs/".$_ENV["current"].".config.json");
-    $_ENV['config'] = json_decode($config);
 
-    if($_ENV["current"] == "dev"){
-        $origin = "http://localhost:3000";
-    }
-    else if($_ENV["current"] == "prod"){
-        $origin = "http://nomdedomaine.com";
-    }
-    
-    header("Access-Control-Allow-Origin: $origin");
-    
-    header("Access-Control-Allow-Methods: *");
-    if($_SERVER['REQUEST_METHOD'] == "OPTIONS"){
-        header('HTTP/1.0 200 OK');
-        die;
-    }
+require_once("vendor/autoload.php");
 
-    require_once 'services/database.service.php';
-    require_once 'controllers/database.controller.php';
+$_ENV["current"] = "dev";
+$config = file_get_contents("configs/" . $_ENV["current"] . ".config.json");
+$_ENV['config'] = json_decode($config);
 
-    $route = trim($_SERVER["REQUEST_URI"], '/');
-    $route = filter_var($route, FILTER_SANITIZE_URL);
-    $route = explode('/', $route);
+if ($_ENV["current"] == "dev") {
+    $origin = "http://localhost:3000";
+} else if ($_ENV["current"] == "prod") {
+    $origin = "http://nomdedomaine.com";
+}
 
-    $controllerName = array_shift($route);
+header('Access-Control-Allow-Headers: Authorization');
+header("Access-Control-Allow-Credentials: true");
 
-    if($_ENV["current"] == "dev" && $controllerName == 'init'){
-        $dbs = new DatabaseService(null);
-        $query_resp = $dbs->query("SELECT table_name FROM information_schema.tables
-                                     WHERE table_schema = ?", ['db_blog']);
-        $rows = $query_resp->statement->fetchAll(PDO::FETCH_COLUMN);
-        foreach($rows as $tableName){
-            $controllerFile = "controllers/$tableName.controller.php";
-            if(!file_exists($controllerFile)){
-                $fileContent = "<?php class ".ucfirst($tableName)
-                    ."Controller extends DatabaseController {\r\n\r\n}?>";
-                file_put_contents($controllerFile, $fileContent);
-                echo ucfirst($tableName)."Controller created\r\n";
-            }
-        }
-        echo 'api initialized';
-        header('HTTP/1.0 200 OK');
-        die;
-    }
+header("Access-Control-Allow-Origin: $origin");
 
-    $controllerFilePath = "controllers/$controllerName.controller.php";
-    if(!file_exists($controllerFilePath)){
-        header('HTTP/1.0 404 Not Found');
-        die;
-    }
-    
-    require_once $controllerFilePath;
-    $controllerClassName = ucfirst($controllerName)."Controller";
-    $controller = new $controllerClassName($route);
 
-    $response = $controller->action;
-    if(!isset($response)){
-        header('HTTP/1.0 404 Not Found');
-        die;
-    }
+header("Access-Control-Allow-Methods: *");
+if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
     header('HTTP/1.0 200 OK');
-    echo json_encode($response);
+    die;
+}
 
-?>
+require_once 'services/database.service.php';
+require_once 'controllers/database.controller.php';
+
+$route = trim($_SERVER["REQUEST_URI"], '/');
+$route = filter_var($route, FILTER_SANITIZE_URL);
+$route = explode('/', $route);
+
+$controllerName = array_shift($route);
+
+if ($_ENV["current"] == "dev" && $controllerName == 'init') {
+    $dbs = new DatabaseService(null);
+    $query_resp = $dbs->query("SELECT table_name FROM information_schema.tables
+                                     WHERE table_schema = ?", ['db_blog']);
+    $rows = $query_resp->statement->fetchAll(PDO::FETCH_COLUMN);
+    foreach ($rows as $tableName) {
+        $controllerFile = "controllers/$tableName.controller.php";
+        if (!file_exists($controllerFile)) {
+            $fileContent = "<?php class " . ucfirst($tableName)
+                . "Controller extends DatabaseController {\r\n\r\n}?>";
+            file_put_contents($controllerFile, $fileContent);
+            echo ucfirst($tableName) . "Controller created\r\n";
+        }
+    }
+    echo 'api initialized';
+    header('HTTP/1.0 200 OK');
+    die;
+}
+
+$controllerFilePath = "controllers/$controllerName.controller.php";
+if (!file_exists($controllerFilePath)) {
+    header('HTTP/1.0 404 Not Found');
+    die;
+}
+
+require_once $controllerFilePath;
+$controllerClassName = ucfirst($controllerName) . "Controller";
+$controller = new $controllerClassName($route);
+
+$response = $controller->action;
+if (!isset($response)) {
+    header('HTTP/1.0 404 Not Found');
+    die;
+}
+header('HTTP/1.0 200 OK');
+echo json_encode($response);
